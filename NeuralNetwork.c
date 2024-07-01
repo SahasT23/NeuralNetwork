5083e0de4298d0c8c845f892640df8354631ee59
SahasT23 <sahas.st01@gmail.com>
2024-07-01 11:26:38 +0100

    changed compiler setting and started with the setup and defining the appropriate functions and methods that we will use to train the network

--- NeuralNetwork.c
+++ NeuralNetwork.c
@@ -1,1 +1,20 @@
-hello
+#include <stdlib.h>
+#include <stdio.h>
+#include <math.h>
+
+// simple neural network to learn the XOR function
+
+#define numInputs 2
+#define numHiddenNodes 2
+#define numOutputs 1
+#define numTrainingSets 4
+
+int main(void) {
+  const double lr = 0.1f;
+
+  double hiddenLayer[numHiddenNodes];
+
+  double outputLayer[numOutputs];
+
+  double hiddenLayerBias[numHiddenNodes];
+}
