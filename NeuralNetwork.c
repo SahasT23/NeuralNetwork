edb80fcc2946ebec3fc30392533a65c3bdbd56bd
SahasT23 <sahas.st01@gmail.com>
2024-07-01 11:56:21 +0100

    Added sigmoid function to assign the weights, as well as derivative of the function and also created a method to shuffle the input arrays

--- NeuralNetwork.c
+++ NeuralNetwork.c
@@ -4,7 +4,23 @@
 
 // simple neural network to learn the XOR function
 
+double sigmoid(double x) {return 1 / (1 + exp(-x)); }
+double dSigmoid(double x) {return x * (1-x); } // sigmoid function differentiated
+
 double init_weights() {return ((double)rand()) / ((double)RAND_MAX);}
+
+void shuffle(int *array, size_t n) {  // passes an array and shuffles the data for new inputs
+
+  if (n > 1){
+    size_t i;
+    for (i = 0; i < n - 1; i++){
+      size_t j = i  + rand() / (RAND_MAX / (n - i) + 1);
+      int t = array[j];
+      array[j] = array[i];
+      array[i] = t;
+    }
+  }
+}
 
 #define numInputs 2
 #define numHiddenNodes 2
