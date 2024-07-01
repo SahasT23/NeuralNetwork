097af4fdce7081c47354fcbeaa613575622ff47e
SahasT23 <sahas.st01@gmail.com>
2024-07-01 11:34:20 +0100

    added code and rearranged the libraries so that they are grouped appropriately and included the desired input and outputs for the XOR function

--- NeuralNetwork.c
+++ NeuralNetwork.c
@@ -13,8 +13,23 @@ int main(void) {
   const double lr = 0.1f;
 
   double hiddenLayer[numHiddenNodes];
-
   double outputLayer[numOutputs];
 
   double hiddenLayerBias[numHiddenNodes];
+  double outputLayerBias[numOutputs];
+
+  double hiddenWeights[numInputs][numHiddenNodes];
+  double outputWeights[numHiddenNodes][numOutputs];
+
+  double training_inputs[numTrainingSets][numInputs] = {{0.0f, 0.0f},
+                                                        {1.0f, 0.0f},
+                                                        {0.0f, 1.0f},
+                                                        {1.0f, 1.0f}};
+
+  double training_inputs[numTrainingSets][numOutputs] = {{0.0f},
+                                                        {1.0f},
+                                                        {1.0f},
+                                                        {0.0f}};
+
+  
 }
